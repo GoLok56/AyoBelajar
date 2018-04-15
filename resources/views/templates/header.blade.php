13f445e9d020a4f154aaeec6e601f5bc26d90bd8
Satria <satadii11@gmail.com>
2018-04-15 10:14:03 +0700

    add header template

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,29 @@
+<?php
+    $menus = [
+        [ "title" => "Home", "link" => "/" ],
+        [ "title" => "Kelas", "link" => "/kelas" ],
+        [ "title" => "Profil", "link" => "/profil" ]
+    ];
+?>
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<title>AyoBelajar</title>
+</head>
+<body>
+    <header>
+        <div class="container flex row" id="header">
+            <h1><a href="/">AyoBelajar</a></h1>
+            <ul>
+                @foreach($menus as $menu)
+                    <li><a href="{{ $menu['link'] }}">{{ $menu['title'] }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </header>
