7d5331a0c07c3a2169d60d0d90ef3d1e315d3606
Satria <satadii11@gmail.com>
2018-04-15 12:10:06 +0700

    add home layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,12 @@
+<div class="flex column">
+
 @include('templates.header')
 
+<main class="container flex column flex-1" id="home">
+    <p class="quotes">"Study as if you were going to live forever. Live as if you were going to die tomorrow"</p>
+    <button class="primary-background">Mulai Belajar</button>
+</main>
+
 @include('templates.footer')
+
+</div>
